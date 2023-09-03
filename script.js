912acdb75834029de013f011e2b455bcf007e79e
allyzahs <work.allyzahsantillan@gmail.com>
2023-09-03 17:00:51 +0800

    Ensure the other buttons are working

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const displayScreen = document.getElementById('input-display')
+const displayOutput = document.getElementById('output-result')
 
-// Displays entered value on screen.
 function displayInput(enteredValue) {
     if (!displayScreen.value) {
       displayScreen.value = "";
     }
     displayScreen.value += enteredValue;
-  }
+    displayOutput.textContent = eval(displayScreen.value);
+}
 
 function resetInputScreen() {
     displayScreen.value = "";
+    displayOutput.textContent = "0"
 }
 
 function deleteInput() {
     let input = displayScreen.value;
     displayScreen.value = input.slice(0, -1);
+    displayOutput.textContent = eval(displayScreen.value);
+}
+
+function displayTotal() {
+    displayScreen.value = displayOutput.textContent
+    displayOutput.textContent = "0"
 }
