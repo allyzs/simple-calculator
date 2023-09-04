794b3093050ce0dcea68ea44fb88b4d3d900f3e7
allyzahs <work.allyzahsantillan@gmail.com>
2023-09-04 11:55:16 +0800

    Made sure to handle consecutive input of operators

--- script.js
+++ script.js
@@ -12,6 +12,13 @@ function displayInput(enteredValue) {
             displayOutput.textContent = eval(displayScreen.value);
         }
     } else {
+        let input = displayScreen.value;
+        let lastInput = input.slice(-1)
+        let lastInputAnOperator = isOperator(lastInput)
+        let enteredValueAnOperator = isOperator(enteredValue)
+        if (lastInputAnOperator && enteredValueAnOperator) {
+            displayScreen.value = input.slice(0, -1);
+        }
         displayScreen.value += enteredValue;
         displayOutput.textContent = eval(displayScreen.value);
     }    
@@ -19,20 +26,24 @@ function displayInput(enteredValue) {
 
 function resetInputScreen() {
     displayScreen.value = "";
-    displayOutput.textContent = "0"
+    displayOutput.textContent = "0";
 }
 
 function deleteInput() {
     let input = displayScreen.value;
     displayScreen.value = input.slice(0, -1);
-    displayOutput.textContent = eval(displayScreen.value);
+    if (displayScreen.value == undefined || displayScreen.value == "") {
+        displayOutput.textContent = "0"
+    } else {
+        displayOutput.textContent = eval(displayScreen.value);
+    }
 }
 
 function displayTotal() {
-    displayScreen.value = displayOutput.textContent
-    displayOutput.textContent = "0"
+    displayScreen.value = displayOutput.textContent;
+    displayOutput.textContent = "0";
 }
 
-function isOperator(enteredValue) {
-    return isNaN(enteredValue)
+function isOperator(value) {
+    return isNaN(value);
 }
